5cf150cd7156222399d34ab1af5e684f7943ed0f
Chidi Tuke <c2kproductions@gmail.com>
2023-10-30 16:15:39 -0400

    added logout function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import { useNavigation } from 'react-router-dom'
+
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './components/Login'
-import JoySignInSideTemplate from './components/Login'
+
 import Register from './components/Register'
-// import Button from "@mui/joy/Button";
+
 
 function App() {
-  // const navigate = useNavigation();
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   navigate("/home");
-  // };
   return (
     <Router>
       <Routes>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,44 @@
-import * as React from 'react'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
-import CssBaseline from '@mui/material/CssBaseline'
-import TextField from '@mui/material/TextField'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
-import Link from '@mui/material/Link'
-import Paper from '@mui/material/Paper'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import micBg from '../images/mic_bg.jpg'
-import GoogleButton from 'react-google-button'
-import SignUpModal from './SignUp'
-import { useState } from 'react'
-import logo from '../images/logo.png'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import micBg from "../images/mic_bg.jpg";
+import GoogleButton from "react-google-button";
+import { useState } from "react";
+import logo from "../images/logo.png";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 function Copyright(props) {
   return (
-    <Typography variant='body2' color='text.secondary' align='center' {...props}>
-      {'Copyright © '}
-      <Link color='inherit' href='https://mui.com/'>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
         PodScribe
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
-  )
+  );
 }
 
-const defaultTheme = createTheme()
+const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false)
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // const handleOpenSignUpModal = () => {
   //   setIsSignUpModalOpen(true)
@@ -48,30 +49,31 @@ export default function SignIn() {
   // }
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
-      const data = new FormData(event.currentTarget)
+      const data = new FormData(event.currentTarget);
+      console.log(data);
       console.log({
-        email: data.get('email'),
-        password: data.get('password'),
-      })
-      const auth = getAuth()
+        email: data.get("email"),
+        password: data.get("password"),
+      });
+      const auth = getAuth();
       const userCred = await signInWithEmailAndPassword(
         auth,
-        data.get('email'),
-        data.get('password')
-      )
+        data.get("email"),
+        data.get("password")
+      );
       if (userCred.user) {
-        navigate('/home')
+        navigate("/home");
       }
     } catch (error) {
-      console.log('ERROR SIGNIN', error)
+      console.log("ERROR SIGNIN", error);
     }
-  }
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component='main' sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -80,11 +82,13 @@ export default function SignIn() {
           md={7}
           sx={{
             backgroundImage: `url(${micBg})`,
-            backgroundRepeat: 'no-repeat',
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -92,9 +96,9 @@ export default function SignIn() {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
             {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar> */}
@@ -102,47 +106,57 @@ export default function SignIn() {
             {/* <Typography component="h1" variant="h5">
               Sign in
             </Typography> */}
-            <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
-                margin='normal'
+                margin="normal"
                 required
                 fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                autoComplete='email'
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
                 autoFocus
               />
               <TextField
-                margin='normal'
+                margin="normal"
                 required
                 fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                autoComplete='current-password'
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
               />
               <FormControlLabel
-                control={<Checkbox value='remember' color='primary' />}
-                label='Remember me'
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
               />
-              <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
                 Sign In
               </Button>
               <GoogleButton
                 onClick={() => {
-                  console.log('Google Button Clicked!')
+                  console.log("Google Button Clicked!");
                 }}
               />
               <Grid container>
                 <Grid item xs>
-                  <Link href='#' variant='body2'>
+                  <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href='/register' variant='body2'>
+                  <Link href="/register" variant="body2">
                     {"Don't have an account? Register"}
                   </Link>
                 </Grid>
@@ -154,5 +168,5 @@ export default function SignIn() {
         </Grid>
       </Grid>
     </ThemeProvider>
-  )
+  );
 }
